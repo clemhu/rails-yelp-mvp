641ed24f253ff49ea0d6abcd683f0010a1e528f2
clement huet <clemhu@hotmail.com>
2019-08-14 17:24:22 +0200

    ex2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([{ name: 'ching-chong', address: '75008', category: 'chinese' },
+{ name: 'olé', address: '75009', category: 'italian' },
+{ name: 'samourai', address: '75010', category: 'japanese' },
+{ name: 'fourchette', address: '75011', category: 'french' },
+{ name: 'frite', address: '75012', category: 'belgian' }])
+puts 'seeded'
